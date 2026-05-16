8fa7cb25a9494023587e0e53267360a91e5f6a75
Aurøra <dreamcorerev@gmail.com>
2026-05-16 05:07:39 +0200

    Update content.php

--- content.php
+++ content.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Eons : Contenu</title>
+<title>Eons - Contenu</title>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css">
 <style>
   :root {
